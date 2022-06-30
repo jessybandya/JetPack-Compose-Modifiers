946047a05d8d41755bb1d8a01f4d6369d9a5b508
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-06-30 15:50:20 +0300

    done.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,41 +3,43 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+             Column(
+                 modifier = Modifier
+                     .background(Color.Green)
+                     .width(300.dp)
+                     .fillMaxHeight(0.5f)
+                     .border(5.dp, Color.Magenta)
+//                     .requiredWidth(300.dp)
+//                     .padding(16.dp)
+                     .padding(top = 16.dp)
+             ) {
+                 Text("Click Me", modifier = Modifier
+                     .offset(80.dp, 20.dp)
+                     .clickable {
+
+                     }
+                 )
+                 Spacer(modifier = Modifier.height(50.dp))
+                 Text("Jessy Bandya!")
+             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
